433f761be1db5e4c99c3827e247f20d9767618e3
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-01-10 16:41:07 +0400

    feat: read 8 bytes with split lines

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goHttp
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./messages.txt")
+	if err != nil {
+		log.Fatalf("Error: could not open file (%v)", err)
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 8)
+
+	str := ""
+	for {
+		n, err := file.Read(buffer)
+		parts := strings.Split(string(buffer[:n]), "\n")
+
+		for _, v := range parts[:len(parts)-1] {
+			fmt.Printf("read: %s\n", str+v)
+		}
+
+		str += parts[len(parts)-1]
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatalf("Error: could not read %d bytes: %v", n, err)
+		}
+	}
+
+	if str != "" {
+		fmt.Printf("read: %s\n", str)
+	}
+}
