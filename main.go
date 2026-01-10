1e37a4c1ab00ce4c5af7d249fa7719f7405eeda3
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-01-10 17:27:49 +0400

    refactor: read lines with channels

--- main.go
+++ main.go
@@ -8,36 +8,51 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	channel := make(chan string)
+	buffer := make([]byte, 8)
+
+	go func() {
+		defer f.Close()
+		defer close(channel)
+		str := ""
+		for {
+			n, err := f.Read(buffer)
+
+			if n > 0 {
+				parts := strings.Split(string(buffer[:n]), "\n")
+				for _, v := range parts[:len(parts)-1] {
+					channel <- str + v
+					str = ""
+				}
+
+				str += parts[len(parts)-1]
+			}
+
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				fmt.Printf("Error: could not read %d bytes: %v", n, err)
+				return
+			}
+		}
+		if str != "" {
+			channel <- str
+		}
+	}()
+
+	return channel
+}
+
 func main() {
 	file, err := os.Open("./messages.txt")
 	if err != nil {
 		log.Fatalf("Error: could not open file (%v)", err)
 	}
-	defer file.Close()
 
-	buffer := make([]byte, 8)
-
-	str := ""
-	for {
-		n, err := file.Read(buffer)
-		parts := strings.Split(string(buffer[:n]), "\n")
-
-		for _, v := range parts[:len(parts)-1] {
-			fmt.Printf("read: %s\n", str+v)
-			str = ""
-		}
-
-		str += parts[len(parts)-1]
-
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Fatalf("Error: could not read %d bytes: %v", n, err)
-		}
-	}
-
-	if str != "" {
-		fmt.Printf("read: %s\n", str)
+	ch := getLinesChannel(file)
+	for v := range ch {
+		fmt.Printf("read: %s\n", v)
 	}
 }
